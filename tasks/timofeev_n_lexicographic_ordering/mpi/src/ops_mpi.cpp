2f99244784b5da9ba19a8fe09820c24c34e9182c
ntimofeev <timohaflekss@gmail.com>
2025-11-23 15:40:52 +0300

    mpi-logic-rework

--- tasks/timofeev_n_lexicographic_ordering/mpi/src/ops_mpi.cpp
+++ tasks/timofeev_n_lexicographic_ordering/mpi/src/ops_mpi.cpp
@@ -24,8 +24,6 @@ bool TimofeevNLexicographicOrderingMPI::PreProcessingImpl() {
 }
 
 bool TimofeevNLexicographicOrderingMPI::RunImpl() {
-  auto input = GetInput();
-
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int size = 0;
@@ -33,6 +31,7 @@ bool TimofeevNLexicographicOrderingMPI::RunImpl() {
   GetOutput() = std::pair<int, int>(1, 1);
 
   if (size <= 1) {
+    auto input = GetInput();
     for (size_t i = 0; !input.first.empty() && i < input.first.length() - 1; i++) {
       GetOutput().first &= static_cast<int>(input.first[i] <= input.first[i + 1]);
     }
@@ -43,14 +42,25 @@ bool TimofeevNLexicographicOrderingMPI::RunImpl() {
   }
 
   if (rank == 0) {
+    auto input = GetInput();
+    size_t second_length = input.second.length();
+    MPI_Send(&second_length, 1, MPI_UNSIGNED_LONG, 1, 0, MPI_COMM_WORLD);
+    MPI_Send(input.second.c_str(), second_length, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
     // only true if comparison is true on every step
     for (size_t i = 0; !input.first.empty() && i < input.first.length() - 1; i++) {
       GetOutput().first &= static_cast<int>(input.first[i] <= input.first[i + 1]);
     }
   } else if (rank == 1) {
-    for (size_t i = 0; !input.second.empty() && i < input.second.length() - 1; i++) {
-      GetOutput().second &= static_cast<int>(input.second[i] <= input.second[i + 1]);
+    size_t llength;
+    // если упадёт на каких-то мудрёных тестах - может быть, дело в size_t
+    MPI_Recv(&llength, 1, MPI_UNSIGNED_LONG, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    char *inpput = new char[(llength + 1) * sizeof(char)];
+    inpput[llength] = '\0';
+    MPI_Recv(inpput, llength, MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    for (size_t i = 0; llength != 0 && i < llength - 1; i++) {
+      GetOutput().second &= static_cast<int>(inpput[i] <= inpput[i + 1]);
     }
+    delete[] inpput;
   }
 
   MPI_Bcast(&GetOutput().first, 1, MPI_INT, 0, MPI_COMM_WORLD);
